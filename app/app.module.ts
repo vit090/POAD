51695aa714ba520f2b294e371cfb0bdf6445936e
Eduardo Zwetsch <42255503+edu060@users.noreply.github.com>
2020-06-15 17:51:35 -0300

    Add files via upload
    sem o arquivo app.component.sass

---
+++ app/app.module.ts
@@ -0,0 +1,27 @@
+import { DeviceMotion } from '@ionic-native/device-motion/ngx';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouteReuseStrategy } from '@angular/router';
+import { ScreenOrientation } from '@ionic-native/screen-orientation/ngx'
+
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+
+@NgModule({
+  declarations: [AppComponent],
+  entryComponents: [],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    DeviceMotion,
+    ScreenOrientation
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
